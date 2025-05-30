049659c62f095dfe70e7e92f8184e8ff8c459996
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-30 12:23:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ menu = st.sidebar.radio("", [
     "Knowledge Assistant",
     "Admin Settings"
 ])
-st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
 st.sidebar.markdown("""
     <div style='padding: 0px 12px 0px 0px;'>
         <img src='https://i.postimg.cc/ydgRHkRq/Comply-Gen-AI-Logo.png' width='250'>
